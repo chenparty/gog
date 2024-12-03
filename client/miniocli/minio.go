67155ba4e1f381236ebe9747daa784e5f33d1c30
chenparty <chen.xiongjian@metiot.com>
2024-12-03 19:10:17 +0800

    feat(minio): add minio client

---
+++ client/miniocli/minio.go
@@ -0,0 +1,100 @@
+package minio_go
+
+import (
+	"context"
+	"github.com/chenparty/gog/zlog"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"io"
+	"time"
+)
+
+var minioClient *minio.Client
+
+type Options struct {
+	AccessKeyID     string
+	SecretAccessKey string
+
+	useSSL bool
+}
+
+type Option func(*Options)
+
+// Connect 连接minio
+func Connect(addr string, options ...Option) {
+	opts := Options{}
+	for _, opt := range options {
+		if opt != nil {
+			opt(&opts)
+		}
+	}
+	minioOptions := minio.Options{
+		Creds:  credentials.NewStaticV4(opts.AccessKeyID, opts.SecretAccessKey, ""),
+		Secure: opts.useSSL,
+	}
+	var err error
+	minioClient, err = minio.New(addr, &minioOptions)
+	if err != nil {
+		zlog.Error().Str("addr", addr).Err(err).Msg("minio连接失败")
+		panic(err)
+	}
+	zlog.Info().Str("addr", addr).Msg("minio连接成功")
+}
+
+// WithAccess 设置访问密钥
+func WithAccess(id, secret string) Option {
+	return func(options *Options) {
+		options.AccessKeyID = id
+		options.SecretAccessKey = secret
+	}
+}
+
+// WithSSL 使用SSL
+func WithSSL(useSSL bool) Option {
+	return func(options *Options) {
+		options.useSSL = useSSL
+	}
+}
+
+// CheckBucket 检查桶是否存在，不存在则创建
+func CheckBucket(ctx context.Context, bucketName string) (err error) {
+	// 检查桶是否存在
+	exists, err := minioClient.BucketExists(ctx, bucketName)
+	if err != nil {
+		return
+	}
+	if !exists {
+		// 创建新桶
+		err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{})
+	}
+	return
+}
+
+// PutObject 上传对象
+func PutObject(ctx context.Context, bucketName, objectName string, reader io.Reader, objectSize int64, contentType string) (err error) {
+	options := minio.PutObjectOptions{ContentType: contentType}
+	_, err = minioClient.PutObject(ctx, bucketName, objectName, reader, objectSize, options)
+	return
+}
+
+// DelObject 删除对象
+func DelObject(ctx context.Context, bucketName, objName string) (err error) {
+	err = minioClient.RemoveObject(ctx, bucketName, objName, minio.RemoveObjectOptions{})
+	return
+}
+
+// PreSignedGetObject 生成带有授权访问的临时URL
+func PreSignedGetObject(ctx context.Context, bucketName, objName string, expiration time.Duration) (url string, err error) {
+	preSignedURL, err := minioClient.PresignedGetObject(ctx, bucketName, objName, expiration, nil)
+	if err != nil {
+		return
+	}
+	url = preSignedURL.String()
+	return
+}
+
+// GetObject 获取对象
+func GetObject(ctx context.Context, bucketName, objName string) (obj *minio.Object, err error) {
+	obj, err = minioClient.GetObject(ctx, bucketName, objName, minio.GetObjectOptions{})
+	return
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/eclipse/paho.mqtt.golang v1.5.0
 	github.com/gin-contrib/requestid v1.0.3
 	github.com/gin-gonic/gin v1.10.0
+	github.com/minio/minio-go/v7 v7.0.81
 	github.com/nats-io/nats.go v1.37.0
 	github.com/oklog/ulid/v2 v2.1.0
 	github.com/redis/go-redis/v9 v9.7.0
@@ -27,6 +28,7 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
@@ -39,11 +41,12 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/nats-io/nkeys v0.4.7 // indirect
@@ -57,11 +60,11 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/arch v0.9.0 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.24.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 	google.golang.org/genproto v0.0.0-20230822172742-b8732ec3820d // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20230822172742-b8732ec3820d // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
