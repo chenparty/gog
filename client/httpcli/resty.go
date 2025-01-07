3a5cf5ead138d85f780c28eddc9fb058048ddac7
chenparty <870300816@qq.com>
2025-01-07 14:02:49 +0800

    improve(httpcli): improve return value

--- client/httpcli/resty.go
+++ client/httpcli/resty.go
@@ -16,12 +16,12 @@ func init() {
 		SetRetryWaitTime(time.Second)
 }
 
-func PostJson(ctx context.Context, url string, body any) (resp *resty.Response, err error) {
+func PostJson(ctx context.Context, url string, body any) (statusCode int, respBody []byte, err error) {
 	req := client.R().SetHeader("Content-Type", "application/json").SetBody(body)
 	zlog.Info().Ctx(ctx).Str("url", url).
 		Any("body", body).
 		Msg("PostJson-Request")
-	resp, err = req.Post(url)
+	resp, err := req.Post(url)
 	if err != nil {
 		zlog.Error().Ctx(ctx).Err(err).Str("url", url).Msg("PostJson error")
 		return
@@ -31,19 +31,23 @@ func PostJson(ctx context.Context, url string, body any) (resp *resty.Response, 
 		Dur("time", resp.Time()).
 		Str("body", string(resp.Body())).
 		Msg("PostJson-Response")
+	statusCode = resp.StatusCode()
+	respBody = resp.Body()
 	return
 }
 
-func Get(ctx context.Context, url string, queryParam map[string]string) (resp *resty.Response, err error) {
+func Get(ctx context.Context, url string, queryParam map[string]string) (statusCode int, respBody []byte, err error) {
 	req := client.R().SetQueryParams(queryParam)
 	zlog.Info().Ctx(ctx).Str("url", url).
 		Any("body", queryParam).
 		Msg("Get-Request")
-	resp, err = req.Get(url)
+	resp, err := req.Get(url)
 	if err != nil {
 		zlog.Error().Ctx(ctx).Err(err).Str("url", url).Msg("Get error")
 		return
 	}
+	statusCode = resp.StatusCode()
+	respBody = resp.Body()
 	zlog.Info().Ctx(ctx).Str("url", url).
 		Str("status", resp.Status()).
 		Dur("time", resp.Time()).
