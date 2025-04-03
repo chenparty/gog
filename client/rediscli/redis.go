dfbc1c1e7da2615d207f9614d2cf82ed35f2388e
chenparty <870300816@qq.com>
2025-04-03 17:01:29 +0800

    feat(client-redis): add universal connect method.

--- client/rediscli/redis.go
+++ client/rediscli/redis.go
@@ -7,14 +7,14 @@ import (
 	"strings"
 )
 
-var redisClient *redis.Client
-var redisClusterClient *redis.ClusterClient
+var redisClient redis.UniversalClient
 
 type Options struct {
 	Username string
 	Password string
 	DB       int
 
+	MasterName       string
 	SentinelUsername string
 	SentinelPassword string
 }
@@ -22,59 +22,21 @@ type Options struct {
 type Option func(*Options)
 
 // Connect 连接redis
-func Connect(addr string, options ...Option) {
+func Connect(addrs []string, options ...Option) {
 	opts := Options{}
 	for _, opt := range options {
 		if opt != nil {
 			opt(&opts)
 		}
 	}
-	redisClient = redis.NewClient(&redis.Options{Addr: addr,
-		Username: opts.Username, Password: opts.Password, DB: opts.DB})
+	uniOpt := &redis.UniversalOptions{
+		Addrs:    addrs,
+		Username: opts.Username, Password: opts.Password, DB: opts.DB,
+		MasterName: opts.MasterName, SentinelUsername: opts.SentinelUsername, SentinelPassword: opts.SentinelPassword,
+	}
+	redisClient = redis.NewUniversalClient(uniOpt)
 	//检测是否连接成功
 	_, err := redisClient.Ping(context.Background()).Result()
-	if err != nil {
-		zlog.Error().Str("addr", addr).Err(err).Msg("redis连接失败")
-		panic(err)
-	}
-	zlog.Info().Str("addr", addr).Msg("redis连接成功")
-}
-
-// ConnectForSentinel 连接redis(哨兵模式)
-func ConnectForSentinel(masterName string, sentinelAddrs []string, options ...Option) {
-	opts := Options{}
-	for _, opt := range options {
-		if opt != nil {
-			opt(&opts)
-		}
-	}
-	redisClient = redis.NewFailoverClient(&redis.FailoverOptions{
-		MasterName:       masterName,
-		SentinelAddrs:    sentinelAddrs,
-		SentinelUsername: opts.SentinelUsername,
-		SentinelPassword: opts.SentinelPassword,
-		Username:         opts.Username, Password: opts.Password, DB: opts.DB})
-	//检测是否连接成功
-	_, err := redisClient.Ping(context.Background()).Result()
-	if err != nil {
-		zlog.Error().Str("addr", strings.Join(sentinelAddrs, ",")).Err(err).Msg("redis连接失败")
-		panic(err)
-	}
-	zlog.Info().Str("addr", strings.Join(sentinelAddrs, ",")).Msg("redis连接成功")
-}
-
-// ConnectForCluster 连接redis(集群模式)
-func ConnectForCluster(addrs []string, options ...Option) {
-	opts := Options{}
-	for _, opt := range options {
-		if opt != nil {
-			opt(&opts)
-		}
-	}
-	redisClusterClient = redis.NewClusterClient(&redis.ClusterOptions{Addrs: addrs,
-		Username: opts.Username, Password: opts.Password})
-	//检测是否连接成功
-	_, err := redisClusterClient.Ping(context.Background()).Result()
 	if err != nil {
 		zlog.Error().Str("addr", strings.Join(addrs, ",")).Err(err).Msg("redis连接失败")
 		panic(err)
@@ -97,9 +59,10 @@ func WithDB(db int) Option {
 	}
 }
 
-// WithSentinelUserAndPass 设置哨兵用户名和密码
-func WithSentinelUserAndPass(user, pwd string) Option {
+// WithSentinel 设置哨兵模式
+func WithSentinel(masterName, user, pwd string) Option {
 	return func(options *Options) {
+		options.MasterName = masterName
 		options.SentinelUsername = user
 		options.SentinelPassword = pwd
 	}
